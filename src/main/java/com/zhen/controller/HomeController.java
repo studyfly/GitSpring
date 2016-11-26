6724558cf50197ca3ecfb758a7f045a84d14ab0c
studyfly <3284728541@qq.com>
2016-11-26 18:09:28 +0800

    demo

---
+++ src/main/java/com/zhen/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.zhen.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/main")
+	public ModelAndView main(HttpServletRequest request){
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("home.html");
+		return mav;
+	}
+}
